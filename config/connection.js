8004e5545f8250a0d4f6e4b27e16f75ad715357b
Darx <mcrivabode@gmail.com>
2022-11-03 11:25:31 -0700

    set up connection.js

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+// initialize sequelize, grab hidden data from .env
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+    }
+);
+
+// export data for use elsewhere
+module.exports = sequelize;
