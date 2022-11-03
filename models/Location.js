9f82de66c74b9464773d95099e7e945da083c20a
Darx <mcrivabode@gmail.com>
2022-11-03 11:54:26 -0700

    set up location model

---
+++ models/Location.js
@@ -0,0 +1,30 @@
+// set up sequelize connections
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Location extends Model {}
+
+Location.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'location',
+    }
+);
+
+// export data for use elsewhere
+module.exports = Location;
